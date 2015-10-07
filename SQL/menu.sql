70d457e1d5992c93a9cf45b52b39f1bdcb36860c
yunus <aydin_10_4@hotmail.com>
2015-10-07 15:55:55 +0200

    database menu veranderd

--- SQL/menu.sql
+++ SQL/menu.sql
@@ -2,10 +2,10 @@
 -- version 4.2.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 02, 2015 at 08:59 AM
--- Server version: 5.6.21-log
--- PHP Version: 5.6.3
+-- Machine: 127.0.0.1
+-- Gegenereerd op: 07 okt 2015 om 15:53
+-- Serverversie: 5.6.21
+-- PHP-versie: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `alap5`
+-- Databank: `alap5`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu`
+-- Tabelstructuur voor tabel `menu`
 --
 
 CREATE TABLE IF NOT EXISTS `menu` (
@@ -36,39 +36,39 @@ CREATE TABLE IF NOT EXISTS `menu` (
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `menu`
+-- Gegevens worden geëxporteerd voor tabel `menu`
 --
 
 INSERT INTO `menu` (`id_nummer`, `product_nummer`, `product_naam`, `product_prijs`, `product_beschrijving`, `product_type`) VALUES
-(1, 101, 'xxl hamburger', 14.95, 'rundvlees, tomaat, ui, kaas, spek, volkoren brood', 'hoofdgerecht'),
-(2, 102, 'medium cheese burger', 11.95, 'kaas, tomaat, ui, volkoren brood', 'hoofdgerecht'),
-(3, 103, 'entrecote', 13.95, 'rundvlees, gemengde groentes', 'hoofdgerecht'),
-(4, 1, 'gambas', 6.95, 'gepelde gambas, gebakken met knoflook en spaanse pepers', 'voorgerecht'),
-(5, 2, 'carpaccio', 7.49, 'carpaccio met truffeldressing en parmezaanse kaas', 'voorgerecht'),
-(6, 3, 'tomatensoep', 4.95, 'tomatensoep met verse gebakken stokbrood', 'voorgerecht'),
-(7, 201, 'creme brulee', 9.95, 'geparfumeerd met citroengras, vanille-ijs en een amandelkrul', 'nagerecht'),
-(8, 202, 'dame blanche', 8.95, 'vanille-ijs met chocolade en slagroomsaus', 'nagerecht'),
-(9, 203, 'tiramisu(18+)', 4.95, 'tiramisu met rum, cacao, mascarpone, koffie en ei', 'nagerecht'),
-(10, 301, 'coca cola', 2.49, 'coca cola met of zonder ijsklontjes', 'dranken'),
+(1, 101, 'XXL Hamburger', 14.95, 'Rundvlees, tomaat, ui, kaas, spek, volkoren brood', 'hoofdgerecht'),
+(2, 102, 'Medium cheese burger', 11.95, 'Kaas, tomaat, ui, volkoren brood', 'hoofdgerecht'),
+(3, 103, 'Entrecote', 13.95, 'Rundvlees, gemengde groentes', 'hoofdgerecht'),
+(4, 1, 'Gambas', 6.95, 'Gepelde gambas, gebakken met knoflook en spaanse pepers', 'voorgerecht'),
+(5, 2, 'Carpaccio', 7.5, 'Carpaccio met truffeldressing, parmezaanse kaas en rucola', 'voorgerecht'),
+(6, 3, 'Tomatensoep', 4.95, 'Tomatensoep met verse gebakken stokbrood', 'voorgerecht'),
+(7, 201, 'Creme brulee', 9.95, 'Geparfumeerd met citroengras, vanille-ijs en een amandelkrul', 'nagerecht'),
+(8, 202, 'Dame blanche', 8.95, 'Vanille-ijs met chocolade en slagroomsaus', 'nagerecht'),
+(9, 203, 'Tiramisu(18+)', 4.95, 'Tiramisu met rum, cacao, mascarpone, koffie en ei', 'nagerecht'),
+(10, 301, 'Coca cola', 2.49, 'Coca cola met of zonder ijsklontjes', 'dranken'),
 (11, 302, '7-up', 2.49, '7-up met of zonder ijsklontjes', 'dranken'),
-(12, 303, 'hertog jan(18+)', 2.99, 'versgebrouwen hertog jan bier vers van de tap', 'dranken');
+(12, 303, 'Hertog jan(18+)', 2.99, 'Versgebrouwen hertog jan bier vers van de tap', 'dranken');
 
 --
--- Indexes for dumped tables
+-- Indexen voor geëxporteerde tabellen
 --
 
 --
--- Indexes for table `menu`
+-- Indexen voor tabel `menu`
 --
 ALTER TABLE `menu`
  ADD PRIMARY KEY (`id_nummer`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
 --
--- AUTO_INCREMENT for table `menu`
+-- AUTO_INCREMENT voor een tabel `menu`
 --
 ALTER TABLE `menu`
 MODIFY `id_nummer` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
